81dde24052f4df60d9c7107569dcecf8af9f1fa5
Abelardo Júnior <abelardo.junior.181@gmail.com>
2023-10-20 12:30:43 -0300

    Add files via upload

---
+++ aula1/main.py
@@ -0,0 +1,8 @@
+usuario = str(input("Digite o seu nome: "))
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+
+
+print(f"Bem vindo {usuario} sua idadeo é {idade} anos, sua altura é {altura} metros")
+
+print("Bem vindo", usuario, "sua idade é", idade,  "anos, sua altura é", altura, "metros")
